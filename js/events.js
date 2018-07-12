d452916468041c0a0124550495de4af90b8470fb
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-07-12 15:10:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('input').on('keydown', function(key) {
+  $('form').on('keydown', function(key) {
     if (key === 71) {
       alert('You pressed the G key.');
       return;
