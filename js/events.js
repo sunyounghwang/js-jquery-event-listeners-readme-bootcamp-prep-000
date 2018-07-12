dd3aafb7cf247ea224ccdf936588ec3490c213c6
Sunyoung Hwang <sunyounghwang13@gmail.com>
2018-07-12 15:13:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt() {
 }
 
 function pressIt() {
-  $('input').on('keypress', function(key) {
+  $('input').on('keydown', function(key) {
     if (key.which == 71) {
       alert('You pressed the G key.');
     }
